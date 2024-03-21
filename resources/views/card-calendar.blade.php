4e8bb2d6a80d524ba588173828fac9c1d4bc99a3
MrMafy <44137539+MrMafy@users.noreply.github.com>
2024-03-21 15:35:57 +0500

    Обновление ветки (21.03.2024 - 15:35)

--- resources/views/card-calendar.blade.php
+++ resources/views/card-calendar.blade.php
@@ -6,7 +6,7 @@
         <div class="row">
             {{-- ЗАГОЛОВОК С ПАНЕЛЬЮ КНОПОК --}}
             <div class="col-md-12 text-left">
-                <h2 class="mb-4"><strong>Карточка объекта</strong></h2>
+                <h2 class="mb-4"><strong>Карточка календаря</strong></h2>
             </div>
             <div class="btns d-flex mb-5">
                 <div class="d-flex gap-2">
