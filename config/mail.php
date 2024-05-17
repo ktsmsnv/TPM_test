e82f8abe7de92d6022f8178f3e03ff49113f88cd
Katherine Samsonova <ktsmsnv@yandex.ru>
2024-05-17 11:58:43 +0500

    отпарвка на почту

--- config/mail.php
+++ config/mail.php
@@ -10,7 +10,7 @@ return [
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', 'mail.kst-energo.ru'),
             'port' => env('MAIL_PORT', 465),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
--- resources/views/emails/workOrderNotification.blade.php
+++ resources/views/emails/workOrderNotification.blade.php
@@ -5,11 +5,11 @@
 </head>
 <body>
 <p>Здравствуйте,</p>
-<p>Создан новый заказ-наряд для обслуживания объекта "{{ $object->name }}" ({{ $object->location }}).</p>
-<p>Тип работы: {{ $service->service_type }}</p>
-<p>Дата обслуживания: {{ $service->planned_maintenance_date }}</p>
-<p>Номер заказа-наряда: {{ $workOrder->number }}</p>
-<p>Статус: {{ $workOrder->status }}</p>
-<p><a href="{{ route('workOrder.show', ['id' => $workOrder->_id]) }}">Просмотреть заказ-наряд</a></p>
+{{--<p>Создан новый заказ-наряд для обслуживания объекта "{{ $object->name }}" ({{ $object->location }}).</p>--}}
+{{--<p>Тип работы: {{ $service->service_type }}</p>--}}
+{{--<p>Дата обслуживания: {{ $service->planned_maintenance_date }}</p>--}}
+{{--<p>Номер заказа-наряда: {{ $workOrder->number }}</p>--}}
+{{--<p>Статус: {{ $workOrder->status }}</p>--}}
+{{--<p><a href="{{ route('workOrder.show', ['id' => $workOrder->_id]) }}">Просмотреть заказ-наряд</a></p>--}}
 </body>
 </html>
