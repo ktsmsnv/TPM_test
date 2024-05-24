d7be2877ef355518c1c3199de75cb24679509686
MrMafy <44137539+MrMafy@users.noreply.github.com>
2024-05-24 16:53:59 +0500

    Обновление ветки 24.05.2024 - 16:53.  - Коммит для теста.

--- resources/views/cards/card-graph-create.blade.php
+++ resources/views/cards/card-graph-create.blade.php
@@ -67,11 +67,10 @@
                                             </div>
                                             <div class="d-flex justify-content-between align-items-center gap-3">
                                                 <label class="w-100">Куратор</label>
-                                                <input name="curator" class="form-control w-100" value="{{ $selectedObjectMain->first()->curator }}" readonly
-                                                       data-toggle="tooltip" title="изменить можно в карточке объекта 'основная'"
+                                                <input name="curator" class="form-control w-100"
+                                                       value="{{ implode(', ', $selectedObjectMain->pluck('curator')->toArray()) }}"
+                                                       readonly data-toggle="tooltip" title="изменить можно в карточке объекта 'основная'"
                                                        placeholder="Введите куратора">
-                                                {{--                                            <input name="curator" value="{{ $selectedObjectMain->first()->curator }}"--}}
-                                                {{--                                                   class="form-control w-100" readonly>--}}
                                             </div>
                                             <div class="d-flex justify-content-between align-items-center gap-3">
                                                 <label class="w-100">Год действия</label>
